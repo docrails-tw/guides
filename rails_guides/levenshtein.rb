3545fdb17d227e29c0161aa9a388b141db14d6a3
Juanito Fatas <katehuang0320@gmail.com>
2015-04-12 14:59:39 +0800

    Speed up Levenshtein by 50% and reduce 97% of memory usage.
    https://github.com/rails/rails/blob/11e8b46a1296889c9c85472a50c6ad6903c021c9/guides/rails_guides/levenshtein.rb

--- rails_guides/levenshtein.rb
+++ rails_guides/levenshtein.rb
@@ -1,7 +1,8 @@
 module RailsGuides
   module Levenshtein
-    # Based on the pseudocode in http://en.wikipedia.org/wiki/Levenshtein_distance
-    def self.distance(s1, s2)
+    # This code is based directly on the Text gem implementation
+    # Returns a value representing the "cost" of transforming str1 into str2
+    def self.distance str1, str2
       s = str1
       t = str2
       n = s.length
@@ -13,10 +14,13 @@ module RailsGuides
       d = (0..m).to_a
       x = nil
 
-      str1.each_char.each_with_index do |char1,i|
+      # avoid duplicating an enumerable object in the loop
+      str2_codepoint_enumerable = str2.each_codepoint
+
+      str1.each_codepoint.with_index do |char1, i|
         e = i+1
 
-        str2.each_char.each_with_index do |char2,j|
+        str2_codepoint_enumerable.with_index do |char2, j|
           cost = (char1 == char2) ? 0 : 1
           x = [
                d[j+1] + 1, # insertion
